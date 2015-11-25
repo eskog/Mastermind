5f3d2825d51465eae1ed56b9cef0573de26b3775
Emil Skog <eskog90@gmail.com>
2015-11-25 20:33:43 +0100

    Refined the guessing process

--- Mastermind.py
+++ Mastermind.py
@@ -3,12 +3,14 @@
 import random
 chosenNumbers = []
 correctGuesses = 0;
+guessCount = 0;
 for x in range(0,4):
     #print(random.randint(0,10))
     chosenNumbers.append(random.randint(0, 9))
 solved = False
 while not solved:
-    guess = raw_input("Enter your guess")
+    correctAnswers = ['-','-','-','-']
+    guess = raw_input("Enter your guess: ")
     try:
         intconvert = int(guess)
     except:
@@ -16,12 +18,15 @@ while not solved:
     else:
         guessedNumber = list(guess)
         for y in range(0,4):
-                #if int(guessedNumber[y]) == chosenNumbers[x]:
             if chosenNumbers[y] == int(guessedNumber[y]):
                 correctGuesses += 1
+                correctAnswers[y] = '*'
         if correctGuesses == 4:
-            print("You have found all the correct numbers")
+            guessCount += 1
+            print("You have found all the correct numbers in {0} guesses").format(guessCount)
             solved = True
         else:
-            print("*" * correctGuesses)
+            #print("*" * correctGuesses)
+            print(correctAnswers)
             correctGuesses = 0
+            guessCount += 1
